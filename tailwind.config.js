41521e6ed5aba3fcf9a20494741f8c92ad5a8ab1
SimonMyrvold <112871818+SimonMyrvold@users.noreply.github.com>
2022-11-23 21:23:33 +0100

    fixed mabyu?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [require('@tailwindcss/forms')],
-};
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('flowbite/plugin')
+  ],
+}
